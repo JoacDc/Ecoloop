3cbbe267f31aaf0f9d9be76d9c649f5b3e852bce
JoacDc <144562822+JoacDc@users.noreply.github.com>
2025-05-21 14:20:40 -0300

    Add files via upload

---
+++ Ecoloop/src/connection.py
@@ -0,0 +1,16 @@
+from pymongo import MongoClient 
+from pymongo.errors import ConnectionFailure 
+
+def getConnection():
+    try:
+        cliente = MongoClient("mongodb://localhost:27017/", serverSelectionTimeoutMS=3000)
+        cliente.admin.command("ping")  # Verificamos conexión con un ping
+        print("✅ Conexión a MongoDB establecida correctamente.")
+        db = cliente["ecoloop"]
+        return db
+    except ConnectionFailure as e:
+        print("❌ Error de conexión a MongoDB:", e)
+        return None
+
+
+
---
+++ Ecoloop/src/gestor.py
@@ -0,0 +1,27 @@
+import random as rd
+from app.Contenedor import Contenedor
+rd.seed()
+
+contenedores = []
+salida = "1"
+while(salida!="0"):
+    id = rd.randint(100,100000)
+    nombre = input("Ingrese el nombre del contenedor: ")
+    print("Coordenadas del conteiner")
+    long = float(input("Longitud: "))
+    lat = float(input("Latitud: "))
+    color = input("Color del contenedor: ")
+    tamaño = float(input("Ingrese el volumen del contenedor (m^3): "))
+    tipoResiduo = input("Tipo de Residuo: ")
+    cantidadMax = int(input("Cantidad Maxima: "))
+    cantidadMin = int(input("Cantidad Minina: "))
+
+    contenedor = Contenedor(id, nombre, lat, long, color, tamaño, tipoResiduo, cantidadMax, cantidadMin)
+
+    contenedores.append(contenedor)
+
+    print("Ingrese cero (0) si desea salir")
+    salida = input()
+
+for cont in contenedores:
+    print(cont)
